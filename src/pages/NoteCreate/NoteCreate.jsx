ee6b1416d9f40246b6fdff88a8560f63be0c6b40
Eva Rodriguez <evarmon@ext.inditex.com>
2024-05-16 13:37:12 +0200

    added setup route and json server

---
+++ src/pages/NoteCreate/NoteCreate.jsx
@@ -0,0 +1,5 @@
+const NoteCreate = () => {
+  return <h1>Note create</h1>
+}
+
+export default NoteCreate
---
+++ src/pages/NoteDetail/NoteDetail.jsx
@@ -0,0 +1,5 @@
+const NoteDetail = () => {
+  return <h1>Note detail</h1>
+}
+
+export default NoteDetail
