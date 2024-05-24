8a00dd85ccce32c73ea9d9efa2cbd8864d5452a8
Eva Rodriguez <evarmon@ext.inditex.com>
2024-05-24 13:15:48 +0200

    added CardComponent

---
+++ src/components/Note/Note.jsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react'
+import styles from './note.module.css'
+import trashGrey from './../../assets/images/trash-grey.png'
+import trashRed from './../../assets/images/trash-red.png'
+
+const Note = ({ title, content, subtitle, onClickNote, onClickTrash }) => {
+  const [isNoteHovered, setIsNoteHovered] = useState(false)
+  const [isTrashHovered, setIsTrashHovered] = useState(false)
+
+  useEffect(() => {
+    console.log('isNoteHovered', isNoteHovered)
+  }, [isNoteHovered])
+
+  useEffect(() => {
+    console.log('isTrashHovered', isTrashHovered)
+  }, [isTrashHovered])
+
+  function onClickTrash_(e) {
+    onClickTrash()
+    e.stopPropagation()
+  }
+  return (
+    <div
+      onClick={onClickNote}
+      className={styles.container}
+      onMouseEnter={() => setIsNoteHovered(true)}
+      onMouseLeave={() => setIsNoteHovered(false)}
+    >
+      <div className={styles.title_row}>
+        <h5 className={styles.title}>Titulo</h5>
+
+        {isNoteHovered && (
+          <img
+            className={styles.trash_icon}
+            src={isTrashHovered ? trashRed : trashGrey}
+            alt="trash"
+            onMouseEnter={() => setIsTrashHovered(true)}
+            onMouseLeave={() => setIsTrashHovered(false)}
+            onClick={onClickTrash_}
+          />
+        )}
+      </div>
+      <h6 className={styles.subtitle}>22/22/22</h6>
+      <p className={styles.text__content}>Contenido</p>
+    </div>
+  )
+}
+
+Note.propTypes = {
+  onClickNote: PropTypes.func,
+  onClickTrash: PropTypes.func,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  content: PropTypes.string,
+}
+
+export default Note
--- src/pages/NoteList/NoteList.jsx
+++ src/pages/NoteList/NoteList.jsx
@@ -1,5 +1,19 @@
+import Note from 'components/Note/Note'
+import { useNavigate } from 'react-router-dom'
+
 const NoteList = () => {
-  return <h1>NoteList</h1>
+  const navigate = useNavigate()
+  return (
+    <div>
+      <Note
+        title="Super note"
+        subtitle="01/01/2022"
+        content="Blabla bla Blabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla bla Blabla bla Blabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla bla Blabla bla Blabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla bla Blabla bla Blabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla bla Blabla bla Blabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla bla Blabla bla Blabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla blaBlabla bla"
+        onClickNote={() => navigate('note/8')}
+        onClickTrash={() => alert('OnClickTrash !')}
+      />
+    </div>
+  )
 }
 
 export default NoteList
