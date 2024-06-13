251e4b444d569c7e87a8f62829227cea3b2d0bd4
Eva Rodriguez <evarmon@ext.inditex.com>
2024-06-13 09:32:52 +0200

    delete note from note list

--- src/pages/NoteList/NoteList.jsx
+++ src/pages/NoteList/NoteList.jsx
@@ -1,11 +1,27 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import Note from 'components/Note/Note'
+import { deleteNoteById, updateNoteById } from 'api/note'
+import { deleteNote, updateNote } from 'store/notes/notes-slice'
+import { useTranslation } from 'react-i18next'
+
 import styles from './noteList.module.css'
 
 const NoteList = () => {
+  const { t } = useTranslation('translator')
+
   const noteList = useSelector(store => store.notesSlice.noteList)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  //XX abstraer esta función
+  async function deleteNote_(note) {
+    if (window.confirm(t('modal.delete-note.contain'))) {
+      deleteNoteById(note.id)
+      dispatch(deleteNote(note))
+      navigate('/')
+    }
+  }
   return (
     <div className={styles.cards_container}>
       {noteList.map(note => (
@@ -14,7 +30,7 @@ const NoteList = () => {
             title={note.title}
             subtitle={note.created_at}
             content={note.content}
-            onClickTrash={() => alert('onClickTrash()')}
+            onClickTrash={() => deleteNote_(note)}
             onClickNote={() => navigate(`note/${note.id}`)}
           />
         </div>
